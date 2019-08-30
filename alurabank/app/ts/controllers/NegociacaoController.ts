f5f60b8b38193709684515d821c4637a0ac4050b
isaacmirandacampos <isaacdmcampos@outlook.com>
2019-08-30 09:05:04 -0300

    Update my code and add Negociacoes.js/ts

--- alurabank/app/ts/controllers/NegociacaoController.ts
+++ alurabank/app/ts/controllers/NegociacaoController.ts
@@ -3,6 +3,7 @@ class NegociacaoController {
     private _inputData: HTMLInputElement;
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
+    private _negociacoes: Negociacoes = new Negociacoes;
 
     constructor() {
 
@@ -11,15 +12,20 @@ class NegociacaoController {
         this._inputValor = <HTMLInputElement> document.querySelector('#valor');
     }
 
-    adiciona(event: Event) {
+    adiciona(event: Event) 
+    {
 
         event.preventDefault();
 
-        const negociacao = new Negociacao(
+        const negociacao: Negociacao = new Negociacao
+        (
             new Date(this._inputData.value.replace(/-/g, ',')),
             parseInt(this._inputQuantidade.value),
-            parseFloat(this._inputValor.value));
+            parseFloat(this._inputValor.value)
+        );
 
-        console.log(negociacao);
+        this._negociacoes.adiciona(negociacao);
+        
+       
     }
 }
---
+++ alurabank/app/ts/models/Negociacoes.ts
@@ -0,0 +1,15 @@
+class Negociacoes {
+
+    private _negociacoes: Array<Negociacao> = [];
+
+    adiciona(negociacao: Negociacao){
+        this._negociacoes.push(negociacao);
+    }
+
+    paraArray(): Negociacao[] {
+        
+        return [].concat(this._negociacoes);
+       
+    }
+};
+let nomes: string[] = [];
