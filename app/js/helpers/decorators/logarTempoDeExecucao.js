a12e87b3c4291be7db3d407db9be47ea891a9f0c
isaacmirandacampos <isaacdmcampos@outlook.com>
2019-09-17 19:10:17 -0300

    Decorator com parâmetro

--- app/js/helpers/decorators/logarTempoDeExecucao.js
+++ app/js/helpers/decorators/logarTempoDeExecucao.js
@@ -1,17 +1,23 @@
 System.register([], function (exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
-    function logarTempoDeExecucao() {
+    function logarTempoDeExecucao(emSegundos = false) {
         return function (target, propertyKey, descriptor) {
             const metodoOrigital = descriptor.value;
             descriptor.value = function (...args) {
+                let unidade = 'ms';
+                let divisor = 1;
+                if (emSegundos) {
+                    unidade = 's';
+                    divisor = 1000;
+                }
                 console.log('-------------------');
                 console.log(`parâmetros passados para o método ${propertyKey}: ${JSON.stringify(args)}`);
                 const t1 = performance.now();
                 const retorno = metodoOrigital.apply(this, args);
                 const t2 = performance.now();
                 console.log(`O retorno do método ${propertyKey} é ${JSON.stringify(retorno)}`);
-                console.log(`O método ${propertyKey} demorou ${t2 - t1} ms`);
+                console.log(`O método ${propertyKey} demorou ${(t2 - t1) / divisor} ${unidade}`);
                 return retorno;
             };
             return descriptor;
--- app/ts/helpers/decorators/logarTempoDeExecucao.ts
+++ app/ts/helpers/decorators/logarTempoDeExecucao.ts
@@ -1,16 +1,22 @@
-export function logarTempoDeExecucao(){
+export function logarTempoDeExecucao(emSegundos: boolean = false){
     
     return function(target: any, propertyKey: string, descriptor: PropertyDescriptor){
         const metodoOrigital = descriptor.value;
 
         descriptor.value = function(...args: any[]){
+            let unidade = 'ms'
+            let divisor = 1;
+            if(emSegundos){
+                unidade = 's';
+                divisor = 1000;
+            }
             console.log('-------------------');
             console.log(`parâmetros passados para o método ${propertyKey}: ${JSON.stringify(args)}`)
             const t1 = performance.now();
             const retorno = metodoOrigital.apply(this, args);
             const t2 = performance.now();
             console.log(`O retorno do método ${propertyKey} é ${JSON.stringify(retorno)}`)
-            console.log(`O método ${propertyKey} demorou ${t2 - t1} ms`)
+            console.log(`O método ${propertyKey} demorou ${(t2 - t1)/divisor} ${unidade}`)
             return retorno;
         }
         return descriptor;
